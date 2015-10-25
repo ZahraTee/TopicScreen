50eed0d1b54e424863086c5b1d483db0463dc9f4
zahra <zahratee@gmail.com>
2015-10-25 08:59:39 +0000

    Hard-coded list of keywords within local storage successfully used to block images.

---
+++ ChromeExt/popup.js
@@ -0,0 +1,65 @@
+console.log("shit");
+saveBlockedTopic();
+
+var wrapper         = $(".input_fields_wrap"); //Fields wrapper
+var add_button      = $(".add_field_button"); //Add button ID
+
+$(add_button).click(function(e){ //on add input button click
+    e.preventDefault();
+        $(wrapper).append('<div><input type="text" name="mytext[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+    }
+});
+
+$(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+    e.preventDefault();
+    deleteBlockedTopic();
+})
+
+function saveBlockedTopic(topic)
+{
+	chrome.storage.local.get("blocked_topics", function(items)
+		{
+			//blocked_topics = items.blocked_topics;
+			blocked_topics = ["girl", "politics", "war"];
+			console.log(items);
+			blocked_topics.push(topic);
+			chrome.storage.sync.set({'blocked_topics': blocked_topics}, function() {
+	          // Notify that we saved.
+	          message('Settings saved');
+	        });
+		})
+	}
+}
+
+function deleteBlockedTopic(topic)
+{
+	StorageArea.get("blocked_topics", function(items)
+		{
+			blocked_topics = items.blocked_topics;
+			console.log(items);
+			blocked_topics.remove(topic);
+			chrome.storage.sync.set({'blocked_topics': blocked_topics}, function() {
+	          // Notify that we saved.
+	          message('Settings saved');
+	        });
+		})
+	}
+}
+
+    var max_fields      = 10; //maximum input boxes allowed
+    var wrapper         = $(".input_fields_wrap"); //Fields wrapper
+    var add_button      = $(".add_field_button"); //Add button ID
+    
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e){ //on add input button click
+        e.preventDefault();
+        if(x < max_fields){ //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div><input type="text" name="mytext[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+        }
+    });
+    
+    $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
+});
---
+++ ChromeExt/scrape.js
@@ -0,0 +1,77 @@
+var i, imgs, token;
+blocked_topics = [];
+saveBlockedTopic("man");
+
+
+function saveBlockedTopic(topic)
+{
+	chrome.storage.sync.get("blocked_topics", function(items)
+	{
+		//blocked_topics = items.blocked_topics;
+		blocked_topics = ["girl", "politics", "war"];
+		blocked_topics.push(topic);
+		chrome.storage.sync.set({'blocked_topics': blocked_topics}, function() {
+         getBlockedTopics();
+        });
+	})
+}
+
+
+function getBlockedTopics()
+{
+	chrome.storage.sync.get("blocked_topics", function(items)
+	{
+		blocked_topics = items.blocked_topics;
+		getToken();
+	})
+	return blocked_topics;
+}
+
+function getToken()
+{
+	$.post("https://api.clarifai.com/v1/token/", {grant_type: "client_credentials", client_id: "0tibu3hLLX0yM_B1IZj_wfIyweqsjhn6w5XRS41O", client_secret: "uS8pDSrBUiR3ZucHLcIrDcU0MMYObopodjU0zmkD"}, function(result){
+        console.log(result);
+        token = result;
+        imgs = $('img');
+		for (i = 0; i < imgs.length; i++) {
+			var alttext = getImageTagsFromURL(blocked_topics, imgs[i], imgs[i].src);
+		}
+	});
+}
+
+function getImageTagsFromURL(blocked, image_element,image_url)
+{
+	if (image_url == null || image_url == "") { return; }
+	var pic_formats = ["jpg", "jpeg", "png", "gif", "bmp", "tiff"];
+	var count;
+	for (var i in pic_formats) { if (endsWith(image_url, pic_formats[i])) count++; }
+	if (count == 0) return;
+
+	$.ajax({
+		url: 'https://api.clarifai.com/v1/tag/',
+		type: 'POST',
+		beforeSend: function (xhr) {
+		    xhr.setRequestHeader('Authorization', 'bearer ' + "kEIZsCcn6Uh1ouFACPka8HPXbKRIqY");
+		},
+		data: {url: image_url},
+		success: function (object) {
+			for (i = 0; i < object.results.length; i++) {
+				var keywords = object.results[i].result.tag.classes;
+				for(x = 0; x < keywords.length; x++)
+				{
+					if (blocked.indexOf(keywords[x]) > -1) {
+						$(image_element).after('<div class="overlay"><h3>Image blocked by TopicScreen</h3></div>');
+						$(image_element).each(function(index) {
+							$(this).next(".overlay").andSelf().wrapAll("<div class='ce-container'/>")
+						});
+					}
+				}
+			}
+		},
+		error: function (err) { return "Unreadable image."},
+	});
+}
+
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
